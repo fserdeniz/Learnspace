36c36430fdd28fab5c0dfbba290d9f7b5812c84e
Fatih Erdeniz <fatiherdeniz@outlook.com>
2023-09-07 15:41:58 +0300

    *ADDED* -FOLDERS: Questions from 8 to 11 -FILES: Questions from 8 to 11 with .md

---
+++ Learn Python/w3resource/basic-1/question-10/q10.py
@@ -0,0 +1,6 @@
+a = int(input("Input an integer: "))
+n1 = int("%s" % a)
+n2 = int("%s%s" % (a,a))
+n3 = int("%s%s%s" % (a,a,a))
+#print(n1 + n2 + n3)
+print("Sum of the %s %s and %s is %s" %(n1,n2,n3,n1+n2+n3))
